7c83b427515bb3e086f747132a0566093c038802
stvbex <12851562+stvbex@users.noreply.github.com>
2020-11-23 20:07:23 +0200

    controllers/blog

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const config = require('./utils/config')
-const Blog = require('./models/blog')
 const http = require('http')
 const express = require('express')
 const app = express()
+const blogsRouter = require('./controllers/blogs')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
@@ -22,23 +22,7 @@ mongoose
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
     console.log(`Server running on port ${config.PORT}`)
